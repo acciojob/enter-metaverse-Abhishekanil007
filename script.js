a71314a44257764d4a27c6bfb9de94afcbffbac6
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-10-28 22:03:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function changeText(){
+	let ptag = document.getElementById("status");
+	
+	let htag = document.createElement("h1");
+	htag.textContent = "Entered Metaverse";
+
+	ptag.parentNode.replaceChild(htag , ptag);
+	
+}
